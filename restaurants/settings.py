b2dd39817fc45592ea7d246ef7187069c8805e81
Ananya Agarwal <ananya.2018@icloud.com>
2026-04-21 17:51:51 -0400

    aws setup to retain uploaded images

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -205,18 +205,20 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = config(
         "AWS_S3_CUSTOM_DOMAIN", default=f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     )
-    AWS_LOCATION = "static"
+    AWS_STATIC_LOCATION = "static"
+    AWS_MEDIA_LOCATION = "media"
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
 
     # S3 Static Settings
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/"
+    STATICFILES_STORAGE = "nomz.storage_backends.StaticStorage"
 
     # S3 Public Media Settings
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/"
+    DEFAULT_FILE_STORAGE = "nomz.storage_backends.PublicMediaStorage"
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = config(
